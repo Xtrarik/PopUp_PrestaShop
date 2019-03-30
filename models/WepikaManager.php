18b1bf0de67cd25b6a8c51ff708bf12de525df7b
Arik <arik.hamdi@gmail.com>
2019-03-30 14:01:27 +0100

    Ajout de index.php dans le dossier Ajax et modification des prefix dans le model manager -> projet 1.0 fini

--- models/WepikaManager.php
+++ models/WepikaManager.php
@@ -1,29 +1,20 @@
 <?php
 class WepikaManager extends ObjectModel
 {
-
+    /**
+     * @param $period_of_time receives a date formatting like '%Y-%m-%d'
+     * @return array containing a single random value between the current date and the one sent in parameter
+     */
     public static function getLastSelling($period_of_time)
     {
-        $sql = "SELECT ps_customer.id_customer As id_customer, ps_customer.id_lang As id_lang, ps_order_detail.product_id As product_id, ps_customer.firstname As firstname,ps_customer.lastname As lastname, ps_address.city AS city, DATE_FORMAT(ps_orders.date_add,'%d-%m-%Y') As date
-              FROM ps_order_detail
-              INNER JOIN ps_orders ON ps_orders.id_order = ps_order_detail.id_order
-              INNER JOIN ps_customer ON ps_customer.id_customer = ps_orders.id_customer
-              INNER JOIN ps_address ON ps_orders.id_address_delivery = ps_address.id_address
-              WHERE DATE_FORMAT(ps_orders.date_add,'%Y-%m-%d') > '" . $period_of_time . " 00:00:00'
-              ORDER BY RAND()
-              Limit 0,1";
+        $p = _DB_PREFIX_;   //get the prefix variable for more flexibility
 
-        return Db::getInstance()->ExecuteS($sql);
-    }
-
-    public static function zzgetLastSelling($period_of_time)
-    {
-        $sql = "SELECT ps_customer.id_customer As id_customer, ps_customer.id_lang As id_lang, ps_order_detail.product_id As product_id, ps_customer.firstname As firstname,ps_customer.lastname As lastname, ps_address.city AS city, DATE_FORMAT(ps_orders.date_add,'%d-%m-%Y') As date
-              FROM ps_order_detail
-              INNER JOIN ps_orders ON ps_orders.id_order = ps_order_detail.id_order
-              INNER JOIN ps_customer ON ps_customer.id_customer = ps_orders.id_customer
-              INNER JOIN ps_address ON ps_orders.id_address_delivery = ps_address.id_address
-              WHERE DATE_FORMAT(ps_orders.date_add,'%Y-%m-%d') > '" . $period_of_time . " 00:00:00'
+        $sql = "SELECT ".$p."customer.id_customer As id_customer, ".$p."customer.id_lang As id_lang, ".$p."order_detail.product_id As product_id, ".$p."customer.firstname As firstname, ".$p."customer.lastname As lastname, ".$p."address.city AS city, DATE_FORMAT(".$p."orders.date_add,'%d-%m-%Y') As date
+              FROM ".$p."order_detail
+              INNER JOIN ".$p."orders ON ".$p."orders.id_order = ".$p."order_detail.id_order
+              INNER JOIN ".$p."customer ON ".$p."customer.id_customer = ".$p."orders.id_customer
+              INNER JOIN ".$p."address ON ".$p."orders.id_address_delivery = ".$p."address.id_address
+              WHERE DATE_FORMAT(".$p."orders.date_add,'%Y-%m-%d') > '" . $period_of_time . " 00:00:00'
               ORDER BY RAND()
               Limit 0,1";
 
